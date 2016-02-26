3edd86e81cd674a1b910fc59d411179b32493145
Rowly <mail.rowlands@gmail.com>
2016-02-26 10:18:24 +0000

    Skeleton complete

--- src/power_switch.py
+++ src/power_switch.py
@@ -8,11 +8,11 @@ from telnet_service import TelnetService
 
 # GUARDS = ["10.10.10.101", "10.10.10.102", "10.10.10.10.3"]
 GUARDS = ["10.10.10.104"]
-TESTING = {"CopperSFP":["10.10.10.151", "10.10.10.152"],
-           "mm2km":["10.10.10.151", "10.10.10.152"],
-           "mm550m":["10.10.10.151", "10.10.10.152"],
-           "sm10km":["10.10.10.151", "10.10.10.152"],
-           "sm30km":["10.10.10.151", "10.10.10.152"]}
+TESTING = {"CopperSFP": ["10.10.10.151", "10.10.10.152"],
+           "mm2km": ["10.10.10.151", "10.10.10.152"],
+           "mm550m": ["10.10.10.151", "10.10.10.152"],
+           "sm10km": ["10.10.10.151", "10.10.10.152"],
+           "sm30km": ["10.10.10.151", "10.10.10.152"]}
 
 def logging_start():
     logging.basicConfig(filename="/var/log/alif-modules/result.log",
@@ -45,11 +45,17 @@ def send_power_restart(ip):
     assert(r.status_code == 200)
 
 
-def telnet_to_alif(ip):
+def telnet_to_alif_fibre_command(ip, command):
     telnet = TelnetService(ip)
-    return telnet.do_dvix_test_command(b"fibre")
+    telnet.do_dvix_test_command(b"fibre phy 1")
+    return telnet.do_dvix_test_command(b"fibre phy 1")
     
-    
+
+def telnet_to_alif_net_command(ip, command):
+    telnet = TelnetService(ip)
+    return telnet.do_dvix_test_command(b"net 8")
+
+
 if __name__ == "__main__":
     executions = 0
     passes = 0
@@ -58,14 +64,28 @@ if __name__ == "__main__":
     for guard in GUARDS:
         send_power_on(guard)
     while True:
-        logging.info("ADDER: Execution {}".format(executions += 1)
+        logging.info("ADDER: Execution {}".format(executions =+ 1)
         for guard in GUARDS:
             send_power_restart(guard)
-        time.sleep(180)
+        time.sleep(300)
         for (k,v) in TESTING:
             logging.info("ADDER: Module {}".format(k))
-            result_a = telnet_to_alif(v[0])
-            result_b - telnet_to_alif(v[1])
+            fibre_result_a = telnet_to_alif_fibre_command(v[0])
+            net_result_a = telnet_to_alif_net_command(v[0])
+            fibre_result_b = telnet_to_alif_fibre_command(v[1])
+            net_result_b = telnet_to_alif_net_command(v[1])
+            fibre_a_flag = "Link UP" in fibre_result_a
+            fibre_b_flag = "Link UP" in fibre_result_b
+            net_a_flag = "SYNC OK; AN OK;" in net_result_a
+            net_b_flag = "SYNC OK; AN OK;" in net_result_b
+            logging.info("ADDER: Fibre - {} {} Net - {} {}".format(fibre_a_flag, fibre_b_flag, net_a_flag, net_b_flag))
+            if fibre_a_flag and fibre_b_flag and net_a_flag and net_b_flag:
+                passes =+ 1
+            else:
+                fails =+ 1
+            logging.info("ADDER: Passes {} Fails {}".format(passes, fails)
+
+            
             
             
         
---
+++ src/telnet_service.py
@@ -0,0 +1,27 @@
+import telnetlib
+
+
+class TelnetService():
+
+    def __init__(self, host):
+        self.host = host
+        self.telnet = telnetlib.Telnet(self.host, 23)
+
+    def login(self):
+        self.telnet.read_until(b"login: ")
+        self.telnet.write(b"root\n")
+        self.telnet.read_until(b"Password: ")
+        self.telnet.write(b"dvix\n")
+
+    def do_dvix_test_command(self, command):
+        self.login()
+        self.telnet.read_until(b"$")
+        self.telnet.write(b"dvix_test\n")
+        self.telnet.read_until(b"test>")
+        self.telnet.write(command + "\n")
+        result = self.telnet.read_all()
+        self.telnet.read_until(b"test>")
+        self.telnet.write(b"q\n")
+        self.telnet.read_until(b"$")
+        self.telnet.write(b"exit\n")
+        return result
